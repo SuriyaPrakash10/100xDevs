e419d52b518aa4d529c737cedfb7d381e1f210b7
Suriya Prakash <102237746+SuriyaPrakash10@users.noreply.github.com>
2024-12-18 23:38:30 +0530

    Delete CourseApp directory

--- CourseApp/config/connectDb.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-const connectDb = () => {
-    mongoose.connect(process.env.DB_URL).then((con)=> {
-        console.log("Mongoose Connected.." + con.connection.host)
-    })
-}
-
-module.exports = connectDb;
